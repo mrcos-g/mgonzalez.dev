854502756ec844d45d9d1701d54363f2662f3c5c
mrcos-g <marcgonzalez@outlook.com>
2024-09-04 14:41:42 -0400

    revert postcss and tailwind config affecting build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,10 +6,7 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    screens: {},
-    extend: {},
-  },
+  theme: {},
   plugins: [],
 };
 export default config;
