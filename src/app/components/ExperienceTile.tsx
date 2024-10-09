99a9c93c3ffa3ccf7006903fc57c2cd1f9c6f9b8
mrcos-g <marcgonzalez@outlook.com>
2024-10-09 14:48:16 -0400

    include experience tile framework

---
+++ src/app/components/ExperienceTile.tsx
@@ -0,0 +1,40 @@
+const ExperienceTile = () => {
+  return (
+    <div className='flex'>
+      <div className='flex-shrink-0 w-32'>
+        <span>2023-2024</span>
+      </div>
+      <div className='flex-1 ml-4'>
+        <h4>
+          <span>Software Developer</span>
+          <span> - </span>
+          <span>Intelitics</span>
+        </h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta a,
+          debitis quisquam fugit sit quas veniam ducimus. Incidunt qui magnam
+          placeat ducimus molestias ea totam facilis deserunt architecto neque,
+          praesentium quae ex culpa, dolore commodi, reiciendis est eius
+          obcaecati quidem dignissimos ipsum laudantium! Iusto et impedit
+          aliquam tempora maxime praesentium ut id vitae! Iste temporibus
+          blanditiis atque quas ex vero quis non quibusdam, maiores tempore
+          laboriosam totam ipsa assumenda illo rem error rerum facilis voluptas!
+        </p>
+        <div>
+          <ul>
+            <li>React</li>
+            <li>Material UI</li>
+            <li>Gatsby.js</li>
+            <li>Node.js</li>
+            <li>GraphQL</li>
+            <li>Postgres</li>
+            <li>AWS</li>
+            <li>Terraform</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExperienceTile;
