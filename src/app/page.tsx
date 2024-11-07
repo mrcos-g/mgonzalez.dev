bae91b65acb9ff553ff0a3b70af65c43a94acbd9
mrcos-g <marcgonzalez@outlook.com>
2024-11-07 14:18:10 -0500

    add links, about, and experience section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,10 +29,9 @@ const Home: FunctionComponent = () => {
           <div>
             <h1 className='text-5xl pb-2 font-bold'>Marcos Gonzalez</h1>
             <h3 className='text-3xl pb-10'>Software Engineer</h3>
-            <p className='text-xl pb-16'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-              deserunt cumque facilis ut eum, animi praesentium omnis fugit
-              ipsam quia!
+            <p className='text-md pb-16 max-w-xs'>
+              Versatile Software Engineer with Full-Stack and Platform focus,
+              driving impactful solutions
             </p>
 
             <ul className='hidden lg:block pl-10'>
@@ -75,83 +74,21 @@ const Home: FunctionComponent = () => {
         </div>
         <div id='main-container' className='pt-24 lg:w-1/2 lg:py-24'>
           <div id='About' className='pb-20'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis
-            voluptatibus maiores fugit porro ipsum temporibus consequatur
-            cumque, voluptatem aperiam nisi, officiis repellendus at et odio
-            nihil quis. Nobis exercitationem odio incidunt, blanditiis,
-            doloremque molestias dolores repellat sit eius sapiente voluptas
-            quisquam labore fuga maxime earum perferendis nisi totam harum
-            impedit quae laborum! Deserunt cum quae neque error dolore, omnis
-            quasi autem voluptas quo iure quos. Obcaecati unde, repellendus
-            accusamus voluptatum veritatis quod fugit corporis perferendis,
-            eveniet quam magni consequatur repudiandae in quo, aspernatur esse
-            animi! Adipisci debitis, eveniet odio natus ex, quis quidem
-            reiciendis aliquam nesciunt excepturi aut ut. Ab dignissimos,
-            deserunt a dolor atque vero optio quo maxime magnam eos placeat,
-            commodi vitae similique, laborum ipsa aspernatur sapiente tempora
-            soluta itaque fugit. Laudantium tempora dolorem fugiat? Quidem
-            quaerat ut blanditiis saepe minus odio provident a nam impedit
-            culpa, cum eaque voluptatibus, tempore odit tempora vero harum est,
-            illo placeat quo distinctio inventore earum. Consequuntur
-            exercitationem accusantium, optio deleniti quisquam similique
-            impedit architecto laboriosam eligendi aliquid, officia quod nulla
-            esse facere quo, aspernatur harum illo veniam? Quasi, ad! Quisquam
-            officia quae dicta tempora. Perspiciatis vel amet, maiores
-            consequuntur provident iure qui possimus. Veniam, dignissimos eos.
-            Rerum, eaque molestiae perferendis accusantium nihil assumenda!
-            Quidem, porro. Maxime vel eos nisi nihil voluptatibus, quos autem
-            sit nostrum id cum quisquam amet ratione omnis tenetur deleniti
-            ducimus animi ab eveniet. Recusandae dicta voluptatum magnam
-            deleniti cum laborum veniam itaque maiores ut alias reprehenderit
-            facilis, autem quod suscipit dignissimos dolorem facere et vel
-            repellendus ex nisi. Eligendi, nesciunt doloremque in voluptate,
-            harum reiciendis maxime asperiores optio iure rem quod omnis culpa
-            aperiam voluptatibus nulla animi dolorum aut pariatur adipisci!
-            Ratione excepturi unde ex tempore iste accusamus suscipit vitae
-            illum ut, doloremque, autem sit debitis aut, amet dolorum delectus
-            sint eos temporibus quod repellendus rem? Maiores!
+            Since 2017, I’ve been on a dynamic journey in software development,
+            beginning with one-page projects and advancing through a coding
+            bootcamp. In 2019, I started my professional coding career at a
+            startup, where I developed my skills in both front-end and back-end
+            development. By 2023, I had grown into a Platform Engineer role in a
+            large corporation, diving deep into cloud infrastructure and
+            comprehensive software systems. My focus now is on leveraging my
+            full-stack and platform experience to deliver solutions across all
+            stages of the development process, always ready to tackle complex
+            challenges.
           </div>
           <div id='Experience' className='pb-20'>
             <ExperienceTile experiences={experiences} />
           </div>
-          <div id='Projects'>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-            vero delectus velit exercitationem, officiis molestiae enim!
-            Deleniti, cupiditate eligendi id, libero quibusdam consequatur
-            labore doloribus dolore delectus eius maiores autem repudiandae
-            voluptates at nam quo sit, suscipit minima. Esse voluptatem alias,
-            quaerat itaque dolorem in odit voluptatibus neque minima. Dolorem
-            nisi ut ea rem unde praesentium similique, autem, quaerat eligendi
-            provident, ex eaque cum rerum. Eos modi id recusandae voluptate
-            ducimus autem amet corrupti numquam omnis debitis perspiciatis
-            doloribus officia doloremque, cumque eveniet fugit ratione
-            temporibus, odio suscipit exercitationem natus? Reiciendis veritatis
-            consectetur quos doloribus, ad, odit nostrum consequuntur, totam
-            ipsam alias minus fugiat labore! Excepturi ea similique obcaecati,
-            ad esse aperiam officia, harum sunt animi, amet repudiandae aliquam
-            modi blanditiis aut corrupti corporis porro illum molestiae?
-            Nesciunt, dolorem esse. Quidem at, deleniti vero, fugiat,
-            repudiandae maiores ipsum voluptates beatae suscipit illo sunt ab
-            fugit. Repellat cumque, temporibus error nisi neque ipsum fugit
-            perspiciatis soluta itaque voluptates similique autem sequi vitae
-            dolore asperiores exercitationem magni doloremque perferendis
-            pariatur. Accusantium nam quibusdam itaque, corrupti ut animi totam
-            labore nobis doloribus odit eius earum impedit delectus architecto
-            quo quia officiis perferendis eos voluptatibus sit cumque sint quos
-            repudiandae. Esse, qui laboriosam aspernatur quasi odit quia!
-            Sapiente perferendis atque officiis omnis enim soluta ipsam
-            veritatis nostrum eligendi molestiae, fugit illo tenetur sequi vel
-            maiores autem aperiam ea? Suscipit sapiente deserunt aspernatur
-            dolorum, praesentium ex adipisci. Facilis deserunt hic asperiores
-            possimus alias quasi amet tenetur dicta quo iusto ipsam officiis
-            reprehenderit sit odio suscipit porro, voluptas voluptates sint
-            blanditiis cum quae! Nam, quaerat, placeat mollitia vero odio
-            reiciendis fuga autem quis quas dolorum vitae. Tenetur adipisci
-            natus doloribus cum! Modi non ipsum autem expedita voluptatibus in
-            doloribus, laboriosam adipisci facilis veniam nulla alias, eaque
-            architecto! Iure velit alias error, quaerat perferendis mollitia
-            iusto cupiditate.
-          </div>
+          <div id='Projects'>Projects Coming Soon...</div>
         </div>
       </div>
     </div>
