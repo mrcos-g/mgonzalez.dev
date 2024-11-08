f74d25dc513ce87a517a486b4a173445a64cc29d
mrcos-g <marcgonzalez@outlook.com>
2024-11-08 15:40:06 -0500

    experience render test and navbar highlight test

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { describe } from 'node:test';
 import { FC } from 'react';
 
@@ -9,11 +9,21 @@ jest.mock('../src/app/components/ScrollLink', () => {
   const MockScrollLink: FC<{ id: string; children: React.ReactNode }> = ({
     id,
     children,
-  }) => (
-    <a href={`#${id}`} data-testid={`scroll-link-${id}`}>
-      {children}
-    </a>
-  );
+  }) => {
+    const isActive = id === 'About';
+    return (
+      <a
+        href={`#${id}`}
+        data-testid={`scroll-link-${id}`}
+        style={{
+          fontWeight: isActive ? 'bold' : 'normal',
+          color: isActive ? 'white' : '#94a3b8',
+        }}
+      >
+        {children}
+      </a>
+    );
+  };
   return MockScrollLink;
 });
 
@@ -25,6 +35,48 @@ jest.mock('../src/app/components/ExperienceTile.tsx', () => {
   return MockExperienceTile;
 });
 
+const mockExperience = [
+  {
+    startYear: 2023,
+    endYear: 2024,
+    role: 'Software Engineer',
+    company: 'Mock Company Two',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, laboriosam maxime nostrum omnis minus exercitationem similique tempore facilis est deleniti.',
+    technologies: ['AWS', 'Terraform', 'Azure Devops', 'Appdynamics', 'Linux'],
+  },
+  {
+    startYear: 2019,
+    endYear: 2023,
+    role: 'Software Engineer',
+    company: 'Mock Company One',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, laboriosam maxime nostrum omnis minus exercitationem similique tempore facilis est deleniti.',
+    technologies: [
+      'Typescript',
+      'React',
+      'Material UI',
+      'Gatsby.js',
+      'Node.js',
+      'GraphQL',
+      'Postgres',
+      'AWS',
+      'Terraform',
+      'Serverless',
+    ],
+  },
+];
+
+beforeEach(() => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({ json: () => Promise.resolve(mockExperience) } as Response)
+  );
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
 describe('Home', () => {
   it('renders header elements and navigation links', () => {
     render(<Home />);
@@ -34,5 +86,22 @@ describe('Home', () => {
     ).toBeInTheDocument();
 
     expect(screen.getByTestId('scroll-link-About')).toHaveTextContent('About');
+  });
+
+  it('renders the experience data fetched', async () => {
+    render(<Home />);
+
+    await waitFor(() =>
+      expect(global.fetch).toHaveBeenCalledWith('/data/experiences.json')
+    );
+
+    expect(screen.getByTestId('experience-tile')).toBeInTheDocument();
+  });
+
+  it('scrolls to the corresponding section when navigation link is clicked', async () => {
+    render(<Home />);
+
+    const aboutLink = screen.getByTestId('scroll-link-About');
+    expect(aboutLink).toHaveStyle('font-weight: bold; color: white');
   });
 });
